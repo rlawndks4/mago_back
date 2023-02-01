4ad368ad14bcdf57a9fa3ecfce3073d48996eb70
rlawndks4 <rlawndks0423@naver.com>
2023-02-01 20:13:39 +0900

    asdf

--- web.js
+++ web.js
@@ -106,9 +106,9 @@ if (is_test) {
 
 } else {
         const options = { // letsencrypt로 받은 인증서 경로를 입력해 줍니다.
-                ca: fs.readFileSync("/etc/letsencrypt/live/1st-academy.kr/fullchain.pem"),
-                key: fs.readFileSync("/etc/letsencrypt/live/1st-academy.kr/privkey.pem"),
-                cert: fs.readFileSync("/etc/letsencrypt/live/1st-academy.kr/cert.pem")
+                ca: fs.readFileSync("/etc/letsencrypt/live/purplevery13.cafe24.com/fullchain.pem"),
+                key: fs.readFileSync("/etc/letsencrypt/live/purplevery13.cafe24.com/privkey.pem"),
+                cert: fs.readFileSync("/etc/letsencrypt/live/purplevery13.cafe24.com/cert.pem")
         };
         server = https.createServer(options, app).listen(HTTPS_PORT, function () {
                 console.log("Server on " + HTTPS_PORT);
